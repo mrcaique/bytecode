40ec2309a198f6991646b577b759617c18c83fad
Caique Marques <mqcaique@gmail.com>
2025-04-17 11:51:11 -0300

    ci: add rpm build to github workspace

--- .github/actions/buildrpm/entrypoint.sh
+++ .github/actions/buildrpm/entrypoint.sh
@@ -6,3 +6,4 @@ mv python-bytecode.spec rpmbuild/SPECS
 mv python-bytecode.tar.gz rpmbuild/SOURCES
 
 rpmbuild -bb rpmbuild/SPECS/python-bytecode.spec
+mv rpmbuild/RPMS/noarch/*.rpm /github/workspace
